36e039f33e1dacffdb51297cfc3c56bab89dc973
Dhruv Sundan <2022a1r118@mietjammu.in>
2025-02-06 12:33:56 +0530

    make auth.js file for authentication

--- server.js
+++ server.js
@@ -2,22 +2,36 @@ const express = require('express');
 const app = express();
 const db = require('./db');
 require('dotenv').config();
+const passport = require('./auth');
+
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000;
 
+// Middleware function
+const logRequest = (req,res,next) =>{
+    console.log(`${new Date().toLocaleString()} Request Made to : ${req.originalURL}`);
+    next();
+}
+
+app.use(logRequest);
 
 
 
-app.get('/', (req,res)=>{
-    res.send("hello world")
+
+app.use(passport.initialize());
+const localAuthMiddleware = passport.authenticate('local',{session:false});
+
+app.get('/',(req,res)=>{
+    res.send("Welcom to our Hotel!");
 
 });
 
 
 
 
+ 
 // Import router files
 const menuRoutes =require("./routes/menuRoutes");
 const personRoutes = require('./routes/personRoutes');
@@ -26,7 +40,7 @@ const personRoutes = require('./routes/personRoutes');
 
 
 //Use the routers
-app.use('/person',personRoutes);
+app.use('/person',localAuthMiddleware,personRoutes);
 app.use('/menu',menuRoutes);
 
 
